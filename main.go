7583c6d877e714d2245ed8f46e171b4603939c4d
Sergio Isair <sergioisair@gmail.com>
2020-10-01 15:25:06 -0500

    Prueba

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var a uint64
+	var b uint64
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	output := (a * b) / 2
+	fmt.Print(output)
 }
